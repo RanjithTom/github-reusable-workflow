76738c2ac412e6c7678ab4437ab87b43f7786644
RanjithTom <ranjithtomy@gmail.com>
2022-04-04 15:36:57 +0200

    Added reusable workflow in github actions

---
+++ function/SampleFunction.java
@@ -0,0 +1,8 @@
+package function;
+
+public class SampleFunction {
+  
+    public static void main(String[] args) {
+        System.out.println("App Running on environment : "+args[0]);
+    }
+  }
